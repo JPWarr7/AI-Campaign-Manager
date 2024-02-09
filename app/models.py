013f369dfb0c6e48a0236ffebb2bf75133e5e357
Diego Aguilar <daaguilar21@gmail.com>
2024-02-09 17:25:11 -0500

    J&D ProofOfConcept w API

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     
     # social media accounts - placeholder boolean for now. API + research
     # necessary to connect social media acct. to our database
