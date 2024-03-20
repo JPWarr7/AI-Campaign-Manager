d22346db7bc70aaeae825b81931cdc96d59a0e5d
Diego Aguilar <daaguilar21@gmail.com>
2024-03-20 10:06:29 -0400

    Added delete_user_data URL and route:
    - added delete_user_data to login.py route
    - created deleteUserData and userDataDeletedSuccessfully html files
    - updated database models to "delete on cascade" and imported "relationship" module to enable cascade deletion
    - debugged "User=None" login error when user did not exist
    - Updated landing.html jinja code to flash Bootstrap messages upon successful account creation, login unsuccessful, and login successful

--- app/database/campaign.py
+++ app/database/campaign.py
@@ -1,19 +1,21 @@
 from app import db
 from datetime import datetime
+from sqlalchemy.orm import relationship
+
 
 class Campaign(db.Model):
-  __tablename__ = 'campaign'
+    __tablename__ = 'campaign'
   
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'))
-  campaign_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  parent_id = db.Column(db.Integer)
-  name = db.Column(db.String(1000), nullable=False)
-  creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id', ondelete='CASCADE'))
+    campaign_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    parent_id = db.Column(db.Integer)
+    name = db.Column(db.String(1000), nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
   
-  links = db.Column(db.String(1000))
-  summarization = db.Column(db.String(2500))
-  perspective = db.Column(db.String(2500))
-  text_generated = db.Column(db.String(2500))
-  image_prompt = db.Column(db.String(2500))
-  image_generated = db.Column(db.String(1000))
+    links = db.Column(db.String(1000))
+    summarization = db.Column(db.String(2500))
+    perspective = db.Column(db.String(2500))
+    text_generated = db.Column(db.String(2500))
+    image_prompt = db.Column(db.String(2500))
+    image_generated = db.Column(db.String(1000))
--- app/database/portfolio.py
+++ app/database/portfolio.py
@@ -1,12 +1,14 @@
 from app import db
 from datetime import datetime
+from sqlalchemy.orm import relationship
+
     
 class Portfolio(db.Model):
     __tablename__ = 'portfolio'
     
     name = db.Column(db.String(50), nullable=False)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     description = db.Column(db.String(1000), nullable=True)
     icon = db.Column(db.String(100), nullable=True)
