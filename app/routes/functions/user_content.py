ec0e7065742601f039f53d06ce78013dc7e3a55f
JPWarr7 <jonathanwarren2022@gmail.com>
2024-04-17 23:10:12 -0400

    minor debugging

--- app/routes/functions/user_content.py
+++ app/routes/functions/user_content.py
@@ -11,17 +11,21 @@ def user_content(user_id):
     portfolios = []
     content = []
     for campaign in all_campaigns:
-        name = campaign.name
-        creation_date = campaign.creation_date
-        links = campaign.links
-        summarization = campaign.summarization
-        perspective = campaign.perspective
-        text_generated = campaign.text_generated
-        image_prompt = campaign.image_prompt
-        image_generated = campaign.image_generated
-        id = campaign.campaign_id
-        portfolio_id = campaign.portfolio_id
-        campaigns.append((name, creation_date, links, summarization, perspective, text_generated, image_prompt, image_generated, id, portfolio_id))
+        if campaign.image_generated is None:
+            db.session.delete(campaign)
+        else:
+            name = campaign.name
+            creation_date = campaign.creation_date
+            links = campaign.links
+            summarization = campaign.summarization
+            perspective = campaign.perspective
+            text_generated = campaign.text_generated
+            image_prompt = campaign.image_prompt
+            image_generated = campaign.image_generated
+            id = campaign.campaign_id
+            portfolio_id = campaign.portfolio_id
+            campaigns.append((name, creation_date, links, summarization, perspective, text_generated, image_prompt, image_generated, id, portfolio_id))
+            
     
     for portfolio in all_portfolios:
         name = portfolio.name
@@ -29,6 +33,7 @@ def user_content(user_id):
         id = portfolio.id
         portfolios.append((name, creation_date, id))
     
+    db.session.commit()
     content.append(portfolios)
     content.append(campaigns)
     return content
